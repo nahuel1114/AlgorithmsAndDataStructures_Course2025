514f01b05fec7b499a9232a590055cd29c89b3ac
Gabriel Valenzuela <gabriel.valenzuela@unc.edu.ar>
2025-04-06 22:49:52 -0300

    add: UI common console colors

---
+++ include/uiDef.hpp
@@ -0,0 +1,13 @@
+#ifndef UI_DEF_HPP
+#define UI_DEF_HPP
+
+#define GREEN  "\033[32m"
+#define BLUE   "\033[34m"
+#define RED    "\033[31m"
+#define WHITE  "\033[37m"
+#define YELLOW "\033[33m"
+#define RESET  "\033[0m"
+
+#define BOLD      "\033[1m"
+#define UNDERLINE "\033[4m"
+#endif // UI_DEF_HPP
